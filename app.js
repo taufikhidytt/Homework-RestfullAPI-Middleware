3640a6af0ad201646e760820ee70ad7dc4a420f8
Taufik Hidayat <hidytt.taufik@gmail.com>
2024-03-07 00:19:28 +0700

    crud routes movies

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = 3000;
 const db = require("./config/db");
 const routerMovie = require("./routes/routeMovies");
 const routerUsers = require("./routes/routeUsers");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/movies", routerMovie);
 app.use("/api/users", routerUsers);
--- routes/routeMovies.js
+++ routes/routeMovies.js
@@ -16,4 +16,53 @@ router.get("/", function (req, res) {
   });
 });
 
+// route get data by id
+router.get("/:id", function (req, res) {
+  const getDataById = `SELECT * FROM movies WHERE id = ${req.params.id}`;
+  db.query(getDataById, function (error, result) {
+    if (error) {
+      throw error;
+    } else {
+      res.status(200).json(result.rows);
+    }
+  });
+});
+
+// route post movies
+router.post("/", function (req, res) {
+  db.query(`INSERT INTO movies ("id", "title", "genres", "year") VALUES ($1, $2, $3, $4)`,
+    [req.body.id, req.body.title, req.body.genres, req.body.year], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).json({
+        status: 'success',
+      });
+    });
+});
+
+// route put movies
+router.put("/:id", function (req, res) {
+  db.query(`UPDATE movies SET title = '${req.body.title}', genres = '${req.body.genres}', year = ${req.body.year} WHERE id = ${req.params.id}`, (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).json({
+        status: 'success',
+      });
+    });
+});
+
+// route deleted movies
+router.delete("/:id", function (req, res) {
+  db.query(`DELETE FROM movies WHERE id = ${req.params.id}`, (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).json({
+        status: 'success',
+      });
+    });
+});
+
 module.exports = router;
