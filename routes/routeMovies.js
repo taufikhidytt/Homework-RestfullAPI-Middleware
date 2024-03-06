fa4b29b965a95170c3e49a8c661aae03942c3db5
taufikhidytt <hidytt.taufik@gmail.com>
2024-03-06 15:31:17 +0700

    add limit

--- routes/routeMovies.js
+++ routes/routeMovies.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
+// route get data dan paginate limit endpoint movies
 router.get("/", function (req, res) {
-  db.query("SELECT * FROM movies", function (error, result) {
+  const getData = `SELECT * FROM movies ${
+    req.query.limit ? "LIMIT " + req.query.limit : ""
+  }`;
+  db.query(getData, function (error, result) {
     if (error) {
       throw error;
     } else {
--- routes/routeUsers.js
+++ routes/routeUsers.js
@@ -2,17 +2,18 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
+// route get data dan paginate limit endpoint users
 router.get("/", function (req, res) {
-  db.query(
-    "SELECT id, email, gender, role FROM users",
-    function (error, result) {
-      if (error) {
-        throw error;
-      } else {
-        res.status(200).json(result.rows);
-      }
+  const getData = `SELECT id, email, gender, role FROM users ${
+    req.query.limit ? "LIMIT " + req.query.limit : ""
+  }`;
+  db.query(getData, function (error, result) {
+    if (error) {
+      throw error;
+    } else {
+      res.status(200).json(result.rows);
     }
-  );
+  });
 });
 
 module.exports = router;
